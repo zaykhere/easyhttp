d77a1ff178b6710d599777a09c93cd7bda10c9f5
zaykhere <52543487+zaykhere@users.noreply.github.com>
2021-03-15 04:24:39 +0500

    redux store alt syntax

--- store.js
+++ store.js
@@ -1,11 +1,13 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const initialState={};
+const reducer = combineReducers({})
+
+const initialState = {};
 
 const middleware = [thunk];
-const store= createStore(rootReducer, initialState, compose(applyMiddleware(...middleware ), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ));
+
+const store= createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
 
 export default store;
-
